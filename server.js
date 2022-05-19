b4d9533a36d198bc43d2d6d7bfc64d704e63891c
Marcus Piri <Marcus.piri@cmeducations.se>
2022-05-19 13:17:53 +0200

    fixed alot of things in the code

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ const { readJson, writeJson, sendJson, sendCode, getId } = require("./utils");
 
 async function sendTodos(req, res, id = null) {
 	let todos = await readJson("todos.json");
+
 	switch (req.method) {
+		case "GET":
+			if (!id) {
+				sendJson(res, todos);
+				return;
+			}
+			const item = todos.find((item) => id === item.id);
+			if (!item) {
+				sendCode(res, 404);
+			} else {
+				sendJson(res, item);
+			}
+			break;
+
 		case "POST":
 			req.on("data", (chunk) => {
 				const data = JSON.parse(chunk.toString());
-				data.id = getId();
-				todos.push(data);
+				todos.push({
+					id: getId(),
+					text: data.text,
+					isCompleted: false,
+					createdAt: Date.now(),
+					completedAt: 0,
+				});
 			});
 			req.on("end", async () => {
 				await writeJson("todos.json", todos);
@@ -17,21 +36,28 @@ async function sendTodos(req, res, id = null) {
 				sendCode(res, 201);
 			});
 			break;
+
 		case "DELETE":
 			todos = todos.filter((todo) => todo.id !== id);
 			writeJson("todos.json", todos).then(() => sendCode(res, 200));
 			break;
-		case "GET":
-			if (!id) {
-				sendJson(res, todos);
-				return;
-			}
-			const item = todos.find((item) => parseInt(id) === item.id);
-			if (!item) {
-				sendCode(res, 404);
-			} else {
-				sendJson(res, item);
-			}
+
+		case "PATCH":
+			req.on("data", (chunk) => {
+				const data = JSON.parse(chunk.toString());
+				const index = todos.findIndex((todo) => todo.id === id);
+
+				if (data.text) {
+					todos[index].text = data.text;
+				}
+				if (typeof data.isCompleted === "boolean") {
+					todos[index].isCompleted = data.isCompleted;
+				}
+				if (typeof data.completedAt === "number") {
+					todos[index].completedAt = data.completedAt;
+				}
+				writeJson("todos.json", todos).then(() => sendCode(res, 200));
+			});
 			break;
 
 		case "PUT":
@@ -52,39 +78,28 @@ async function sendTodos(req, res, id = null) {
 			});
 			break;
 
-		case "PATCH":
-			req.on("data", (chunk) => {
-				const data = JSON.parse(chunk.toString());
-				const index = todos.findIndex((todo) => todo.id === id);
-
-				if (data.text) {
-					todos[index].text = data.text;
-				}
-				if (typeof data.isCompleted === "boolean") {
-					todos[index].isCompleted = data.isCompleted;
-				}
-				writeJson("todos.json", todos).then(() => sendCode(res, 200));
-			});
-			break;
 		case "OPTIONS":
-			res.setHeader("Access-Control-Allow-Origin", "*");
-			res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-			sendCode(res, 200);
-
+			sendCode(res, 204);
 			break;
 
 		default:
+			sendCode(res, 400);
 			break;
 	}
 }
 
 const server = http.createServer((req, res) => {
 	const [endPoint, id] = req.url.split("/").filter((item) => item.length > 0);
-
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, PATCH, DELETE, OPTIONS, POST, PUT"
+	);
 	console.log(`${req.method} till url: ${req.url}`);
 
 	if (endPoint === "todos") {
-		sendTodos(req, res, parseInt(id));
+		sendTodos(req, res, id);
 	} else {
 		sendCode(res, 404);
 	}
--- utils.js
+++ utils.js
@@ -1,39 +1,28 @@
 const fs = require("fs/promises");
+const crypto = require("crypto");
 
 async function readJson(path) {
-	try {
-		const buffer = await fs.readFile(path);
-		return JSON.parse(buffer.toString());
-	} catch (error) {
-		throw error;
-	}
+	const buffer = await fs.readFile(path);
+	return JSON.parse(buffer.toString());
 }
 
 async function writeJson(path, data) {
-	try {
-		const json = JSON.stringify(data);
-		await fs.writeFile(path, json);
-	} catch (error) {
-		throw error;
-	}
+	const json = JSON.stringify(data);
+	await fs.writeFile(path, json);
 }
 
 function getId() {
-	return Number(
-		new Date().getTime().toString() + Math.floor(Math.random() * 1000000)
-	);
+	return crypto.randomBytes(6).toString("hex");
 }
 
 function sendJson(res, data) {
 	res.writeHead(200, {
 		"Content-Type": "application/json",
-		"Access-Control-Allow-Origin": "*",
 	});
 	res.end(JSON.stringify(data));
 }
 
 function sendCode(res, code = 500) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.statusCode = code;
 	res.end();
 }
